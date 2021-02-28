89b363f31a7b190bde8ffeb724a2a50c40df4023
Ryan Inouye <inouyeryan29@gmail.com>
2021-02-27 15:32:13 -1000

    Initial Commit

---
+++ LearningGH/LearningGHApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningGHApp.swift
+//  LearningGH
+//
+//  Created by Ryan Inouye on 2/27/21.
+//
+
+import SwiftUI
+
+@main
+struct LearningGHApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
